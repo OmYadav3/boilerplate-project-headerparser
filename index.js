33893394ad058565a3892e59a53f7c73ee3e0c19
Om Yadav <omydv3@gmail.com>
2024-04-12 00:31:35 +0530

    make a route for ip address

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
@@ -14,17 +10,21 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
+
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const ipaddressObj = req.query.ip;
+  res.json({ ipaddress: ipaddressObj });
 });
 
-// listen for requests :)
+
+
+
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
